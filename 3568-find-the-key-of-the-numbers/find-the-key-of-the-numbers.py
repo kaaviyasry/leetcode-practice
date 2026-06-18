13d7fca55c65241e27dca44cb31b17bb285570d6
Kaaviya <155509061+kaaviyasry@users.noreply.github.com>
2026-06-18 13:51:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (18.92%) - LeetSync

--- 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.py
+++ 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def generateKey(self, num1, num2, num3):
-        num1 = str(num1).zfill(4)
-        num2 = str(num2).zfill(4)
-        num3 = str(num3).zfill(4)
-
-        ans = ""
-
+        n1=str(num1).zfill(4)
+        n2=str(num2).zfill(4)
+        n3=str(num3).zfill(4)
+        ans=""
         for i in range(4):
-            ans += min(num1[i], num2[i], num3[i])
-
+            ans+=min(n1[i],n2[i],n3[i])
         return int(ans)
 
 
