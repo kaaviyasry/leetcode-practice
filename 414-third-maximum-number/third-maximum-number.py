b591a509e1ac760044afddc0d40ff0bfdccf348a
Kaaviya <155509061+kaaviyasry@users.noreply.github.com>
2026-06-24 09:26:58 +0530

    Time: 1 ms (43.30%) | Memory: 20.3 MB (57.93%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.py
+++ 414-third-maximum-number/third-maximum-number.py
@@ -3,7 +3,7 @@ class Solution:
         nums=list(set(nums))
         nums.sort()
         if len(nums)<3:
-            return nums[-1]
+            return nums[-1]  
         return nums[-3]
 
         
