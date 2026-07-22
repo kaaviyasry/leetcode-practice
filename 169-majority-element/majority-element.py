9b60f6ae4ae466bbeff7117e1cda4030d520581e
Kaaviya <155509061+kaaviyasry@users.noreply.github.com>
2026-07-22 10:21:08 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (19.11%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,4 +1,5 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        return max(set(nums),key=nums.count)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[len(nums)//2]
         
